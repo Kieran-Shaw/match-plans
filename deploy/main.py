7e3a4ca62bc7c2e210b519927c8328149aba5920
Kieran Shaw <71037357+Kieran-Shaw@users.noreply.github.com>
2024-03-25 15:23:54 -0700

    commenting out name
    * commenting out name

--- deploy/main.py
+++ deploy/main.py
@@ -55,5 +55,5 @@ def predict():
     # Return the top 5 predicted plans as a JSON response
     return jsonify({'predictions': top_predicted_plans})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5001)
